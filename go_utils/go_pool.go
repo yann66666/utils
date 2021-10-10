858e3c76167d8c8a674518d4a93e2c92ec7cdecf
yann <yanf@bdatacloud.cn>
2021-10-10 20:37:14 +0800

    新功能: go_pool: go协程池

---
+++ go_utils/go_pool.go
@@ -0,0 +1,104 @@
+// Author: yann
+// Date: 2021/10/10 3:21 下午
+// Desc:
+
+package utils
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+// 如果传入任务超时返回的错误信息
+var ErrScheduleTimeout = fmt.Errorf("schedule error: timed out")
+
+// 线程池模型
+type Pool struct {
+	sem  chan struct{} //线程池队列
+	work chan func()   //任务队列
+	min  uint32        //最小线程数数量
+}
+
+//GoroutinePoolOptions 协程池初始化参数
+type GoroutinePoolOptions struct {
+	MaxSize   uint32 //MaxSize 协程最大数量
+	QueueSize uint32 //QueueSize 队列
+	CSize     uint32 //CSize 当前/最小协程数量
+}
+
+//***************************************************
+//Description : 初始化线程池
+//param :       GoroutinePoolOptions选项参数
+//return :      线程池
+//***************************************************
+func NewPool(opt *GoroutinePoolOptions) *Pool {
+	if opt.CSize <= 0 {
+		panic("The current number of threads must be greater than or equal to zero")
+	}
+	if opt.QueueSize <= 0 {
+		panic("The task queue size must be greater than zero")
+	}
+	if opt.CSize > opt.MaxSize {
+		panic("The current number of threads must be less than the maximum number of threads")
+	}
+	p := &Pool{
+		sem:  make(chan struct{}, opt.MaxSize),
+		work: make(chan func(), opt.QueueSize),
+		min:  opt.CSize,
+	}
+	for i := uint32(0); i < opt.CSize; i++ {
+		p.sem <- struct{}{}
+		go p.worker(func() {})
+	}
+
+	return p
+}
+
+//***************************************************
+//Description : 传入任务
+//param :       任务句柄
+//***************************************************
+func (p *Pool) Schedule(task func()) {
+	_ = p.schedule(task, nil)
+}
+
+//***************************************************
+//Description : 传入任务设置超时
+//param :       超时时间
+//param :       任务句柄
+//return :      如果任务队列和协程队列均满一直无法写入,超过时间返回超时错误
+//***************************************************
+func (p *Pool) ScheduleTimeout(timeout time.Duration, task func()) error {
+	return p.schedule(task, time.After(timeout))
+}
+
+func (p *Pool) schedule(task func(), timeout <-chan time.Time) error {
+	select {
+	case <-timeout:
+		return ErrScheduleTimeout
+	case p.work <- task: //如果任务队列未满传入任务
+		return nil
+	case p.sem <- struct{}{}: //如果任务队列已满,创建协程
+		go p.worker(task)
+		return nil
+	}
+}
+
+//线程工作
+func (p *Pool) worker(task func()) {
+	defer func() { <-p.sem }()
+
+	task()
+	//循环获取任务, 当超时后判断当前线程数量是否大于最小线程数量, 大于则退出本线程
+	for {
+		select {
+		case f := <-p.work:
+			f()
+		case <-time.After(time.Minute):
+			if uint32(len(p.sem)) > atomic.LoadUint32(&p.min) {
+				return
+			}
+		}
+	}
+}
