183892abdeb862d7b8a9bc89874673b95cc5686e
yann <yanf@bdatacloud.cn>
2021-10-15 11:44:05 +0800

    新功能: IsGtZero: 判断各种类型是小于等于0

--- field_utils/number_util.go
+++ field_utils/number_util.go
@@ -73,3 +73,37 @@ func IsLtZero(number interface{}) bool {
 		return false
 	}
 }
+
+//是否小于等于0
+func IsLtOrEqZero(number interface{}) bool {
+	switch number.(type) {
+	case int64:
+		return number.(int64) <= 0
+	case uint64:
+		return number.(uint64) <= 0
+	case int:
+		return number.(int) <= 0
+	case uint:
+		return number.(uint) <= 0
+	case int32:
+		return number.(int32) <= 0
+	case uint32:
+		return number.(uint32) <= 0
+	case int8:
+		return number.(int8) <= 0
+	case uint8:
+		return number.(uint8) <= 0
+	case float32:
+		return number.(float32) <= 0
+	case float64:
+		return number.(float64) <= 0
+	case string:
+		num, err := strconv.ParseFloat(number.(string), 64)
+		if err != nil {
+			return false
+		}
+		return num <= 0
+	default:
+		return false
+	}
+}
